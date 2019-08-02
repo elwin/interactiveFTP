e2cf8ee9e06b742f613025873ead4c5f30f127a9
Elwin Stephan <elwin.stephan@gmail.com>
2019-08-02 09:51:17 +0200

    Addded keepalive

--- main.go
+++ main.go
@@ -2,30 +2,25 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"strings"
+	"time"
 
 	ftp "github.com/elwin/transmit2/client"
 )
 
 func main() {
-
-	commands["help"] = app.help
-
-	if err := run(); err != nil {
-		fmt.Println(err)
+	app := App{
+		ctx: context.Background(),
 	}
 
-}
-
-type commandMap map[string]func([]string)
-
-var (
-	app      = App{}
-	commands = commandMap{
+	app.cmd = commandMap{
+		"help":    app.help,
 		"connect": app.connect,
 		"login":   app.login,
 		"ls":      app.ls,
@@ -35,23 +30,40 @@ var (
 		"get":     app.retr,
 		"put":     app.stor,
 	}
-	local = flag.String("local", "1-ff00:0:112,[127.0.0.1]:4000", "Local hostname (default: 1-ff00:0:112,[127.0.0.1]:4000")
+
+	if err := app.run(); err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+type commandMap map[string]func([]string)
+
+var (
+	local    = flag.String("local", "", "Local hostname (default: 1-ff00:0:112,[127.0.0.1]:4000")
+	interval = time.Duration(15 * time.Second)
 )
 
 func init() {
 	flag.Parse()
+	if *local == "" {
+		log.Fatalf("Please set the local address with -local")
+	}
 }
 
 type App struct {
-	conn *ftp.ServerConn
-	out  io.Writer
+	conn   *ftp.ServerConn
+	out    io.Writer
+	cmd    commandMap
+	ctx    context.Context
+	cancel context.CancelFunc
 }
 
 func (app *App) print(a interface{}) {
 	fmt.Fprintln(app.out, a)
 }
 
-func run() error {
+func (app *App) run() error {
 	scanner := bufio.NewReader(os.Stdin)
 	app.out = os.Stdout
 
@@ -63,7 +75,7 @@ func run() error {
 		}
 
 		args := strings.Split(strings.TrimSpace(input), " ")
-		if f, ok := commands[args[0]]; ok {
+		if f, ok := app.cmd[args[0]]; ok {
 			if app.conn == nil && args[0] != "help" && args[0] != "connect" {
 				app.print("Need to make a connection first")
 				continue
@@ -76,7 +88,7 @@ func run() error {
 }
 
 func (app *App) help(args []string) {
-	for cmd := range commands {
+	for cmd := range app.cmd {
 		app.print(cmd)
 	}
 }
@@ -93,6 +105,28 @@ func (app *App) connect(args []string) {
 	}
 
 	app.conn = conn
+
+	ctx, cancel := context.WithCancel(app.ctx)
+	app.cancel = cancel
+
+	go app.keepalive(ctx, interval)
+}
+
+func (app *App) keepalive(ctx context.Context, interval time.Duration) {
+
+	for {
+		select {
+		case <-time.After(interval):
+			err := app.conn.NoOp()
+			if err != nil {
+				app.print(fmt.Sprintf("Failed to ping for keepalive: %s", err))
+				return
+			}
+		case <-ctx.Done():
+			return
+		}
+	}
+
 }
 
 func (app *App) login(args []string) {
@@ -199,6 +233,17 @@ func (app *App) stor(args []string) {
 	}
 
 	err = app.conn.Stor(args[1], f)
+	if err != nil {
+		app.print(err)
+	}
+}
+
+func (app *App) quit(args []string) {
+	if app.cancel != nil {
+		app.cancel()
+	}
+
+	err := app.conn.Quit()
 	if err != nil {
 		app.print(err)
 	}
